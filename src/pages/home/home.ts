83bfec3479aacf94cdcddbad97c9169c56ead44d
anupamkarn <anupam.karn.9726@gmail.com>
2018-03-31 13:51:51 +0530

    initial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+//import { IonicPage } from 'ionic-angular'
 import { NavController } from 'ionic-angular';
+import {ElementRef} from '@angular/core'
 
+declare var google;
+//@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('map') mapElement: ElementRef;
+  start : any;
+  end : any;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  calculateAndDisplayRoute() {
+    
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
+    
+    directionsService.route({
+      origin: this.start,
+      destination: this.end,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
   }
 
 }
